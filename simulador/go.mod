23cfa649369a045d06cfe3f315b17b44242fdda9
thiago_srib <thiago.srib@gmail.com>
2023-02-07 16:11:05 -0300

    feat: add simulator

---
+++ simulador/go.mod
@@ -0,0 +1,9 @@
+module github.com/thiagosrib/imersaofullcycle2-simulator
+
+go 1.16
+
+require (
+	github.com/confluentinc/confluent-kafka-go v1.6.1
+	github.com/confluentinc/confluent-kafka-go/v2 v2.0.2 // indirect
+	github.com/joho/godotenv v1.3.0
+)
---
+++ simulador/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	kafka2 "github.com/thiagosrib/imersaofullcycle2-simulator/application/kafka"
+	"github.com/thiagosrib/imersaofullcycle2-simulator/infra/kafka"
+	// ckafka "github.com/confluentinc/confluent-kafka-go/kafka"
+	ckafka "github.com/confluentinc/confluent-kafka-go/v2/kafka"
+	"github.com/joho/godotenv"
+	"log"
+)
+
+func init() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("error loading .env file")
+	}
+}
+
+func main() {
+	// // para enviar msg, é utilizado o códito abaido
+	// // para ficar lendo a msg pelo console do kafka, entrar no container do kafka (kafka-kafka-1) e digita: 
+	// //     kafka-console-consumer --bootstrap-server=localhost:9092 --topic=route.new-direction
+	// producer := kafka.NewKafkaProducer()
+	// kafka.Publish("ola", "route.new-direction", producer)
+
+	// // é necessário esse loop infinito para que dê tempo de o kafka receber a msg
+	// for {
+	// 	_ = 1
+	// }
+
+	// para receber msg, é utilizado o códito abaido
+	// // para enviar uma msg direto pelo console do kafka executar o comando abaixo:
+	// //    kafka-console-producer --bootstrap-server=localhost:9092 --topic=route.new-direction
+	msgChan := make(chan *ckafka.Message)
+	consumer := kafka.NewKafkaConsumer(msgChan)
+	go consumer.Consume()
+
+	for msg := range msgChan {
+		fmt.Println(string(msg.Value))
+		go kafka2.Produce(msg)
+	}
+}
